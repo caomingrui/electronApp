a239d1f1afac7580edc763986c2694cfb3267b28
caomingrui <1529539548@qq.com>
2021-01-27 17:42:10 +0800

    提交的说明注释

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    let win = new BrowserWindow({
+        // 具体参数 http://www.niuguwen.cn/kaifashouce-cat-138-13802.html
+        width: 400,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('./dist/index.html');
+}
+
+app.whenReady().then(createWindow);
